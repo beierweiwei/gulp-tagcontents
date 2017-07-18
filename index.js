098a21cf5f4f6eb179b9ccb9729ada342d218b14
beierweiwei <364704059@qq.com>
2017-07-18 21:56:35 +0800

    delet blankline

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const PLUGIN_NAME = 'gulp-tagcontents';
 function gulpGetStyle(option){
 		var tag = option && option.tag ? option.tag : 'style'; 
 		return through2.obj(function(file, enc , cb){
-
 			if(file.isNull()){
 				cb(null, file);
 			}
@@ -16,14 +15,9 @@ function gulpGetStyle(option){
 				var pattern = '<\\s*' + tag + '[^>]*?>((.|\\n|\\r)*?)<\\s*\\/\\s*' + tag + '\\s*>';
 				var match = new RegExp(pattern, 'ig');
 				var styles = contentStr.match(match);
-		
 				if(!styles) {this.push(null); return cb()};
-
 				styles = styles.map(function(item, i){
-					
 					return item.match(new RegExp(pattern, 'i'))[1];
-
-
 				});
 				styles.filter(function(item, i){
 					return !!item;
@@ -36,7 +30,6 @@ function gulpGetStyle(option){
 				var error = 'Streaming not supported';
         return cb(new PluginError(PLUGIN_NAME, error));
 			}
-
 		});
 }
 
